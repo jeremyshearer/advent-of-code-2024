27da2439fe3f94335e5d10fbbfd46b7e9623187b
Jeremy Shearer <jeremyshearer@icloud.com>
2024-12-11 21:42:06 -0600

    day eleven part 2

---
+++ go/cmd/day11b/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := os.ReadFile("./cmd/day11b/sample.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	hallway := map[int]int{}
+	for _, v := range strings.Fields(string(input)) {
+		stoneValue, _ := strconv.Atoi(v)
+		hallway[stoneValue] = 1
+	}
+	for i := range 75 {
+		newHallway := map[int]int{}
+		for s, c := range hallway {
+			newStones := blink(s)
+			for _, n := range newStones {
+				newHallway[n] += c
+			}
+		}
+		hallway = newHallway
+		fmt.Println(i)
+	}
+	var total int
+	for _, count := range hallway {
+		total += count
+	}
+	fmt.Printf("%d\n", total)
+}
+
+func blink(val int) []int {
+	if val == 0 {
+		return []int{1}
+	} else if evenDigitCount(val) {
+		return split(val)
+	} else {
+		return multiply(val)
+	}
+}
+
+func split(val int) []int {
+	digitStr := strconv.Itoa(val)
+	digits := []string{digitStr[:len(digitStr)/2], digitStr[len(digitStr)/2:]}
+	firstStoneVal, _ := strconv.Atoi(digits[0])
+	secondStoneVal, _ := strconv.Atoi(digits[1])
+	return []int{firstStoneVal, secondStoneVal}
+}
+
+func multiply(val int) []int {
+	return []int{val * 2024}
+}
+
+func evenDigitCount(val int) bool {
+	valString := strconv.Itoa(val)
+
+	return len(valString)%2 == 0
+}
